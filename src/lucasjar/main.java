3a0c361a51b146bf0694f37391531bfe79d9817b
aluno <jonatassev@github.com>
2022-07-13 20:02:52 -0300

    Primeiro commit

---
+++ src/lucasjar/main.java
@@ -0,0 +1,11 @@
+package lucasjar;
+
+public class main {
+    
+    public static void main(String[] args) {
+        DistanciaEuclidiana obj = new DistanciaEuclidiana();
+        obj.calcular(200);          
+    }
+                
+    
+}
